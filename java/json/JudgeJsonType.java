e1344cd3a5c28d8b005164e651b0bdbf3f93ba87
unirithe <71979722+unirithe@users.noreply.github.com>
2021-11-18 13:54:44 +0800

    Update JudgeJsonType.java

--- java/json/JudgeJsonType.java
+++ java/json/JudgeJsonType.java
@@ -1,1 +1,11 @@
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONException;
 
+public void boolean isJson(String data){
+  try {
+    JSON.parse(data);
+    return true;
+  } catch(JSONException e){
+    return false;
+  }
+}
